727c90188850e3f0e081a5f749d356beac9b9970
liubvg <47124339+liubvg@users.noreply.github.com>
2022-10-12 19:04:31 +0200

    Add files via upload

---
+++ susmongus.py
@@ -0,0 +1,16 @@
+a = int(input("Please input your first number  "))
+b = int(input("Please input your second number  "))
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+g = a//b
+h = a%b
+i = a**b
+print ("Sum",c)
+print ("Diffrence",d)
+print ("Multiplication",e)
+print ("Division",f)
+print ("Integer Division",g)
+print ("Remainder",h)
+print ("Exponent",i)
